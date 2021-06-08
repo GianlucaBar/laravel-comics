be0e2b070ff8b2dcda93c482425e5d2d102d7c7e
GianlucaBar <gianlucabarce@gmail.com>
2021-06-08 16:54:10 +0200

    footer complete

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,139 @@
 <footer>
-    qualcosa
+    <div class="blue-section">
+        <div class="container-standard">
+            <ul class="blue-menu">
+                <li>
+                    <img class="list-icon" src="{{ asset('img/buy-comics-digital-comics.png') }}" alt=""> <span>DIGITAL COMICS</span>
+                </li>
+
+                <li>
+                    <img class="list-icon" src="{{ asset('img/buy-comics-merchandise.png') }}" alt=""> <span>DC MERCHANDISE</span>
+                </li>
+
+                <li>
+                    <img class="list-icon" src="{{ asset('img/buy-comics-subscriptions.png') }}" alt=""> <span>SUBSCRIPTION</span>
+                </li>
+
+                <li>
+                    <img class="list-icon" src="{{ asset('img/buy-comics-shop-locator.png') }}" alt=""> <span>COMIC SHOP LOCATOR</span>
+                </li>
+
+                <li>
+                    <img class="list-icon power-visa" src="{{ asset('img/buy-dc-power-visa.svg') }}" alt=""> <span>DC POWER VISA</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="bg-section">
+        <div class="container-standard flex-container-centered">
+            <div class="footer-menu-container">
+                <div class="flex-container">
+                    <div class="footer-block">
+                        <ul class="footer-menu">
+                            <li> <h3>DC COMICS</h3></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                        </ul>
+
+                        <ul class="footer-menu">
+                            <li> <h3>SHOP</h3></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="footer-block">
+                        <ul class="footer-menu">
+                            <li> <h3>DC COMICS</h3></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                        </ul>
+                    </div>
+
+                    <div class="footer-block">
+                        <ul class="footer-menu">
+                            <li> <h3>DC COMICS</h3></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                            <li><a href="#">Characters</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="copiright">
+                    <span>All Site Content TM and c 2020 DC Entertainment, unless otherwhise <a href="#">noted here</a>.   All rights reserved.</span>
+                    <div><a href="#">Cookies Settings</a></div>
+                </div>
+            </div>
+
+            <div class="big-logo">
+                <img src="{{ asset('img/dc-logo-bg.png') }}" alt="big logo DC">
+            </div>
+        </div>
+    </div>
+
+    <div class="social-section">
+        <div class="container-standard">
+            <button>SIGN-UP NOW!</button>
+
+            <ul class="social-menu flex-container-centered">
+                <li>
+                    <h3>FOLLOW US</h3>
+                </li>
+
+                <li> <img src="{{ asset('img/footer-facebook.png') }}" alt=""></li>
+
+                <li> <img src="{{ asset('img/footer-twitter.png') }}" alt=""></li>
+
+                <li> <img src="{{ asset('img/footer-youtube.png') }}" alt=""></li>
+
+                <li> <img src="{{ asset('img/footer-pinterest.png') }}" alt=""></li>
+
+                <li> <img src="{{ asset('img/footer-periscope.png') }}" alt=""></li>
+            </ul>
+        </div>
+    </div>
 </footer>
