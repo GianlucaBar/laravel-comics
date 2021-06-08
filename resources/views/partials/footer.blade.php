bbc09ad7234c733afe806ce8862fc833d89e5cd5
GianlucaBar <gianlucabarce@gmail.com>
2021-06-08 14:43:51 +0200

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    qualcosa
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,72 @@
+<header>
+    <div class="top-menu">
+        <div class="container-standard">
+            <ul>
+                <li>
+                    <a href="#">DC power VISA</a>
+                </li>
+    
+                <li>
+                    <a href="#">ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="container-standard">
+        <nav class="top-nav">
+            <div class="logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="">
+            </div>
+
+            <ul class="header-menu">
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+
+                <li>
+                    <a href="#">TV</a>
+                </li>
+
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+
+                <li>
+                    <a href="#">SHOP <i class="fas fa-caret-down"></i></a>
+                </li>
+
+            </ul>
+
+            <div class="search-bar">
+                <input type="text" placeholder="Search">
+            </div>
+        </nav>
+    </div>
+
+
+</header>
